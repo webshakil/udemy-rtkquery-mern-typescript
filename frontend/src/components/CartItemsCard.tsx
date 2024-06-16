30000684c60c76fce09469c2454f3245fa99296e
webshakil <chumkysk@gmail.com>
2024-06-17 05:49:32 +0600

    class-65-CartPageFunctionality

--- frontend/src/components/CartItemsCard.tsx
+++ frontend/src/components/CartItemsCard.tsx
@@ -1,31 +1,40 @@
 import { AiFillDelete } from 'react-icons/ai'
+import { CartItem } from '../types/types';
+import { server } from '../redux/store';
 
-
-
-const CartItemsCard = ()=>{
-    
+type CartItemProps = {
+  cartItem: CartItem;
+  incrementHandler: (cartItem: CartItem) => void;
+  decrementHandler: (cartItem: CartItem) => void;
+  removeHandler: (id: string) => void;
+};
+const CartItemsCard = ({cartItem,
+  incrementHandler,
+  decrementHandler,
+  removeHandler}:CartItemProps) => {
+    const { photo, productId, name, price, quantity } = cartItem;
   return (
     <>
      <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
-              src=""
-              alt=""
+              src={`${server}/${photo}`}
+              alt={name}
               className="w-full rounded-lg sm:w-40"
             />
             <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
               <div className="mt-5 sm:mt-0">
-                <h2 className="text-lg font-bold text-gray-900 ">Name</h2>
-                <p className="mt-1 text-2xl text-gray-700">Price</p>
+                <h2 className="text-lg font-bold text-gray-900 ">{name}</h2>
+                <p className="mt-1 text-2xl text-gray-700">${price}</p>
               </div>
               <div className="mt-4 flex flex-col items-center sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                 <div className="flex items-center border-gray-100">
-                  <span  className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                  <span onClick={() => decrementHandler(cartItem)} className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
                 
-                  <p>quantity</p>
-                  <span  className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                  <p>{quantity}</p>
+                  <span onClick={() => incrementHandler(cartItem)} className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
                  
                 </div>
-                <button  className="mt-2 text-red-500 hover:text-red-700 text-2xl"><AiFillDelete /></button>
+                <button onClick={() => removeHandler(productId)} className="mt-2 text-red-500 hover:text-red-700 text-2xl"><AiFillDelete /></button>
               </div>
             </div>
           </div>
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,36 +1,114 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, server } from "../redux/store";
+import { addToCart, calculatePrice, discountApplied, removeCartItem } from "../redux/reducer/cartReducer";
+import { CartItem } from "../types/types";
+import { useEffect, useState } from "react";
 import CartItemsCard from "../components/CartItemsCard";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const CartPage = () => {
-   return (
-       <div className="h-screen bg-gray-100 pt-20">
+  const { cartItems, subtotal, tax, total, shippingCharges, discount } =
+  useSelector((state: RootState) => state.cartReducer);
+const dispatch = useDispatch();
+
+const [couponCode, setCouponCode] = useState<string>("");
+const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+const incrementHandler = (cartItem: CartItem) => {
+  if (cartItem.quantity >= cartItem.stock) return;
+
+  dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity + 1 }));
+};
+const decrementHandler = (cartItem: CartItem) => {
+  if (cartItem.quantity <= 1) return;
+
+  dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity - 1 }));
+};
+const removeHandler=(productId:string)=>{
+  dispatch(removeCartItem(productId))
+}
+
+useEffect(() => {
+  const { token: cancelToken, cancel } = axios.CancelToken.source();
+
+  const timeOutID = setTimeout(() => {
+    axios
+      .get(`${server}/api/v1/payment/discount?coupon=${couponCode}`, {
+        cancelToken,
+      })
+      .then((res) => {
+        dispatch(discountApplied(res.data.discount));
+        setIsValidCouponCode(true);
+        dispatch(calculatePrice());
+      })
+      .catch(() => {
+        dispatch(discountApplied(0));
+        setIsValidCouponCode(false);
+        dispatch(calculatePrice());
+      });
+  }, 1000);
+
+  return () => {
+    clearTimeout(timeOutID);
+    cancel();
+    setIsValidCouponCode(false);
+  };
+}, [couponCode]);
+useEffect(() => {
+  dispatch(calculatePrice());
+}, [cartItems]);
+  return (
+
+    <div className="h-screen bg-gray-100 pt-20">
       <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
-        <CartItemsCard/>
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => (
+            <CartItemsCard
+              incrementHandler={incrementHandler}
+              decrementHandler={decrementHandler}
+              removeHandler={removeHandler}
+              key={idx}
+              cartItem={i}
+            />
+          ))
+        ) : (
+          
+          <>    <div className="flex flex-col items-center justify-center bg-lime-400">
+          <h1 className="text-3xl font-bold text-black mb-4">
+            No Items Added
+          </h1>
+          <p className="text-lg text-black-200">
+            Please add items to continue.
+          </p>
+        </div></>
+        )}
+      
         </div>
-       
+        {/* Sub total */}
         <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
           <div className="mb-2 flex justify-between">
             <p className="text-gray-700">Subtotal</p>
-            <p className="text-gray-700">subtotal</p>
+            <p className="text-gray-700">${subtotal}</p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-700">Shipping Charges</p>
-            <p className="text-gray-700">shippingCharges</p>
+            <p className="text-gray-700">${shippingCharges}</p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-700">Tax</p>
-            <p className="text-gray-700">tax</p>
+            <p className="text-gray-700">${tax}</p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-700">Discount</p>
-            <p className="text-gray-700">discount</p>
+            <p className="text-gray-700">-${discount}</p>
           </div>
           <hr className="my-4" />
           <div className="flex justify-between">
             <p className="text-lg font-bold">Total</p>
             <div className="">
-              <p className="mb-1 text-lg font-bold">total</p>
+              <p className="mb-1 text-lg font-bold">${total}</p>
               <p className="text-sm text-gray-700">including VAT</p>
             </div>
         
@@ -39,18 +117,20 @@ const CartPage = () => {
                 type="text"
                  placeholder="Coupon Code"
                  className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition-all duration-300"
-                
+                 value={couponCode}
+                  onChange={(e) => setCouponCode(e.target.value)}
         /></div>
-          
+          {couponCode &&
+          (isValidCouponCode ? (
             <span className="green">
-              Discount off using the <code>couponCode</code>
+              ${discount} off using the <code>{couponCode}</code>
             </span>
-        
+          ) : (
             <span className="red">
-              
+              Invalid Coupon 
             </span>
-        
-          <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
+          ))}
+          {cartItems.length > 0 && <Link to="/dashboard/user/shipping"><button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button></Link>}
          
         </div>
       </div>
@@ -59,4 +139,5 @@ const CartPage = () => {
 };
 
 export default CartPage;
+
 
